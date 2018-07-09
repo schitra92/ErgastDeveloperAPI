cd82b68a5c7cac2b21aea23114980af54de9d637
schitra92 <schitra192@gmail.com>
2018-07-09 10:07:05 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
-//import { Component, Inject } from '@angular/core';
+
 import {MatSelectModule} from '@angular/material/select';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Component, Injectable } from '@angular/core';
-
-
-//import {Observable} from "rxjs/Observable";
-//import "rxjs/Rx";
 
 @Component({
   selector: 'app-root',
@@ -13,12 +9,6 @@ import { Component, Injectable } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-// private url: 'http://ergast.com/api/f1/drivers.json?callback=myParser';
-//public apps: Applications[];
-
 constructor() {
-
 }
-
-  //console.log("here:"+ this.myData);
 }
